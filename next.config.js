b15b51df8bfa0cc2a7894828f938566337554a22
ANDRÉS BENVENUTO <56841920+andresbenve@users.noreply.github.com>
2022-03-31 19:42:06 -0300

    second commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "fakestoreapi.com"],
+  },
+  env: {
+    stripe_public_key: process.env.STRIPE_PUBLIC_KEY,
+  },
+};
